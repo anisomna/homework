2bfa04f42c6774b236b6bc624029021de79a6ee5
Kovaleva Marina <kovalevamarina516@gmail.com>
2025-04-17 15:15:22 +0400

    feat: add func to delete the node of the tree

--- tree3.cpp
+++ tree3.cpp
@@ -140,6 +140,65 @@ tree* Prev(tree* tr, int x) {
     return y; // возвращаем родителя
 }
 
+void delete_node(tree *&tr, tree *v){
+    tree *p = v->parent;
+
+    if (!p) tr = NULL; //дерево содержит один узел
+    else if (!v->left && !v->right){ //если нет детей
+        if (p->left == v) //указатель у родителя меняем на NULL
+            p->left = NULL;
+        if (p->right == v)
+            p->right = NULL;
+        delete v;
+    }
+
+    else if (!v->left || !v->right){ //если только один ребенок
+        if (!p) { //если удаляем корень, у которого 1 ребенок
+            if (!v->left){ //если есть правый ребенок
+                tr = v->right; //он становится корнем
+                v->parent = NULL;
+            }
+            else { //аналогично для левого
+                tr = v->left;
+                v->parent = NULL;
+            }
+        }
+        else {
+            if (!v->left){ //если есть правый ребенок
+                if (p->left == v) //если удаляемый узел явл. левым ребенком
+                    p->left = v->right; //ребенок удаляемого узла становится левым ребенком своего "деда"
+                else
+                    p->right = v->right; //ребенок удаляемого узла становится правым ребенком своего "деда"
+                v->right->parent = p; //родителем ребенка становится его "дед"
+            }
+            else { //аналогично для левого ребенка
+                if (p->left == v)
+                    p->left = v->left;
+                else
+                    p->right = v->left;
+                v->left->parent = p;
+            }
+            delete v;
+        }
+    }
+
+    else { //есть оба ребенка
+        tree *succ = Next(tr, v->inf); //следующий за удаляемым узлом
+        v->inf = succ->inf; //присваиваем значение
+        if (succ->parent->left == succ){ //если succ левый ребенок
+            succ->parent->left = succ->right; //его правый ребенок становится левым ребенком своего "деда"
+            if (succ->right) //если этот ребенок существует
+                succ->right->parent = succ->parent; //его родителем становится "дед"
+        }
+        else { //аналогично если succ - правый ребенок
+            succ->parent->right = succ->right;
+            if (succ->right)
+                succ->right->parent = succ->parent;
+        }
+        delete succ;
+    }
+}
+
 // 10
 // 8 6 4 5 7 10 9 11 3 2
 int main() {
