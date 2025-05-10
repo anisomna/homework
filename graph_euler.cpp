74a7702e8e47fa7664b5d18666c226c9448672cc
Kovaleva Marina <kovalevamarina516@gmail.com>
2025-05-10 16:05:21 +0400

    feat: add func to find euler path/cycle

--- graph_euler.cpp
+++ graph_euler.cpp
@@ -65,11 +65,97 @@ void del_edges(vector<vector<int>> &vc, int x, int y) {
     del_el(vc[y], x);
 }
 
+void euler(vector<vector<int>>& gr, int n) {
+    vector<int> degrees(n, 0); // находим степени вершин
+    for (int i = 0; i < n; i++) {
+        degrees[i] = gr[i].size();
+    }
+
+    int v1 = -1, v2 = -1;
+    for (int i = 0; i < n; i++) {
+        if (degrees[i] % 2 != 0) {
+            if (v1 == -1) v1 = i;
+            else if (v2 == -1) v2 = i;
+            else {
+                cout << "no path, no cycle" << endl;
+                return;
+            }
+        }
+    }
+
+    bool eu_cycle = (v1 == -1 && v2 == -1);
+    bool eu_path = (v1 != -1 && v2 != -1);
+
+    if (!eu_cycle && !eu_path) {
+        cout << "no path, no cycle" << endl;
+        return;
+    }
+
+    // если есть путь, добавляем фиктивное ребро для превращения его в цикл
+    bool add_edge = false;
+    if (eu_path) {
+        gr[v1].push_back(v2);
+        gr[v2].push_back(v1);
+        degrees[v1]++;
+        degrees[v2]++;
+        add_edge = true;
+    }
+
+    stack<int> st;
+    vector<int> res;
+
+    int start;
+    if (eu_path) start = v1;
+    else start = 0;
+    st.push(start);
+
+    while (!st.empty()) {
+        int cur = st.top();
+        if (!gr[cur].empty()) {
+            int next = gr[cur].back();
+            st.push(next);
+            del_edges(gr, cur, next);
+            degrees[cur]--;
+            degrees[next]--;
+        }
+        else {
+            res.push_back(cur);
+            st.pop();
+        }
+    }
+
+    // если было добавлено фиктивное ребро, удаляем его из пути
+    if (add_edge) {
+        for (int i = 0; i + 1 < res.size(); ++i) {
+            if ((res[i] == v1 && res[i + 1] == v2) || (res[i] == v2 && res[i + 1] == v1)) {
+                vector<int> temp(res.begin() + i + 1, res.end());
+                temp.insert(temp.end(), res.begin() + 1, res.begin() + i + 1);
+                res = temp;
+                break;
+            }
+        }
+    }
+
+    if (eu_cycle) cout << "euler cycle: ";
+    else cout << "euler path: ";
+
+    bool fl = true;
+    for (int i : res) {
+        if (fl) {
+            cout << i;
+            fl = false;
+        }
+        else cout << " -> " << i;
+    }
+    cout << endl;
+}
+
 int main() {
     input();
     print(gr);
     cout << endl;
     int n = gr.size();
+    euler(gr, n);
 
     return 0;
 }
