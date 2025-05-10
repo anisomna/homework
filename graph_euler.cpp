403be22596e98fed74b030ce08ee047b8ce34b0c
Kovaleva Marina <kovalevamarina516@gmail.com>
2025-05-10 16:00:51 +0400

    feat: add func to create adjacency list of graph from file

---
+++ graph_euler.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <stack>
+#include <unordered_set>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> gr;
+bool orient = false; // неориентированный граф
+unordered_set<int> vertices;
+
+void input() {
+    ifstream in("input.txt");
+    int x, y;
+    int max_vertex = -1;
+    
+    while (in >> x >> y) { // вводим ребро в виде x -> y
+        vertices.insert(x);
+        vertices.insert(y);
+        max_vertex = max(max_vertex, max(x, y));
+    }
+    gr.resize(max_vertex + 1);
+    
+    in.clear();
+    in.seekg(0);
+
+    while (in >> x >> y) { // вводим ребро в виде x -> y
+        if (find(gr[x].begin(), gr[x].end(), y) == gr[x].end()) { // проверяем существует ли ребро x -> y
+            gr[x].push_back(y); // добавляем x -> y
+            if (!orient) { // неориентированный
+                gr[y].push_back(x); // добавляем y -> x
+            }
+        }
+    }
+}
+
+int main() {
+    input();
+    cout << endl;
+    int n = gr.size();
+
+    return 0;
+}
